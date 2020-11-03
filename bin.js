f55cb772ad5a94627ca533a39631a2c2f6e8c5fb
younganJung1115 <youngan.jung@gmail.com>
2020-11-03 07:49:45 +0000

    change

---
+++ bin.js
@@ -0,0 +1,5 @@
+const getPage = require('.') /* the current working directory so that means main.js because of package.json */
+// let theFile = process.argv[2] /* what the user enters as first argument */
+
+getPage()
+
--- getNewsWriteDocs.js
+++ getNewsWriteDocs.js
@@ -80,5 +80,3 @@ const getPage = () => {
             });
         });
 }
-
-getPage()
